600cf949dddd60eafaf81f5151a2bcd90bbafaac
airforce270 <96109779+airforce270@users.noreply.github.com>
2022-10-15 22:43:38 -0700

    Add git pull to startup script

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+git pull
 docker compose -f ../docker-compose.yml -f ../docker-compose.prod.yml pull
 docker compose -f ../docker-compose.yml -f ../docker-compose.prod.yml down
 docker compose -f ../docker-compose.yml -f ../docker-compose.prod.yml up -d --no-build
